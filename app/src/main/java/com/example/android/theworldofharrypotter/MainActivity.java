cbf1d5728a7069469d62d70a4bfce24431b63bf4
Theda Rogers <crayonwriter@gmail.com>
2018-06-24 22:41:09 -0400

    Initial commit with ViewPager defined in Java and XML Main Actvities

---
+++ app/src/main/java/com/example/android/theworldofharrypotter/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.android.theworldofharrypotter;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Find the viewpager
+        ViewPager viewPager = findViewById(R.id.viewPager);
+    }
+}
