91b89d55fa8f92344c211766bb09a455fc7a5672
Theda Rogers <crayonwriter@gmail.com>
2018-06-29 14:43:50 -0400

    Added data to the FamousLocations arraylist.

--- app/src/main/java/com/example/android/theworldofharrypotter/FamousLocationsFragment.java
+++ app/src/main/java/com/example/android/theworldofharrypotter/FamousLocationsFragment.java
@@ -29,11 +29,11 @@ public class FamousLocationsFragment extends Fragment {
 
         // Create a list of Creature entities
         final ArrayList<Entity> entity = new ArrayList<Entity>();
-        entity.add(new Entity("Mountain Troll", "The largest and most aggressive of the trolls.", R.drawable.griffin));
-        entity.add(new Entity("Winged Horse", "Thestrals are a type of winged horse.", R.drawable.griffin));
-        entity.add(new Entity("Grindylow", "This water demon is aggressive, but some mer-people keep them as pets.", R.drawable.grindylow));
-        entity.add(new Entity("Hippogriff ", "Buckbeak is a famous Hippogriff, part eagle, part horse.", R.drawable.griffin));
-        entity.add(new Entity("Phoenix", "This red bird bursts into flames on Burning Day, rises from the ashes, and is full-sized in days.", R.drawable.phoenix));
+        entity.add(new Entity("Quidditch World Cup Campsite", "The Muggle campsite where wizards camped out before the Quidditch World Cup."));
+        entity.add(new Entity("12 Grimmauld Place", "Originally the home of the Black family, this Muggle-invisible house became a location for the Order of the Phoenix."));
+        entity.add(new Entity("The Burrow", "The Weasley family home.", R.drawable.grindylow));
+        entity.add(new Entity("Forbidden Forest", "The dark forest by Hogwarts that was forbidden to students."));
+        entity.add(new Entity("Gringotts Wizarding Bank", "Run by Goblins (who also own it), this bank is the only bank for wizards."));
 
         // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
         // adapter knows how to create layouts for each item in the list, using the
@@ -41,7 +41,7 @@ public class FamousLocationsFragment extends Fragment {
         // This list item layout contains a single {@link TextView}, which the adapter will set to
         // display a single word.
 
-        EntityAdapter adapter = new EntityAdapter(getActivity(), entity, R.color.creatures);
+        EntityAdapter adapter = new EntityAdapter(getActivity(), entity, R.color.locations);
 
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
         // There should be a {@link ListView} with the view ID called list, which is declared in the
